3b998f6fdfa0108f6f0a5f8e4e500ec7a0555c71
Dylan Kite <dylan@Kristens-Mac-mini.local>
2020-01-21 19:56:00 -0800

    add part of second module, begin phone component

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,5 +1,7 @@
 const CyberBullying = require("./cyber-bullying-module.json")
+const CyberSafety = require("./cyber-safety-module.json")
 
 module.exports = {
+    CyberSafety,
     CyberBullying
 }
