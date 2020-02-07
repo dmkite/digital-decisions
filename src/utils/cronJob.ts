9be14b7af11c19c3b46a53e04dc020c76f9951c4
Kite <e48f@M-C02X73SVJHD2.nordstrom.net>
2020-02-07 13:48:36 -0800

    add storage accessing for repsonses and errors

--- src/utils/cronJob.ts
+++ src/utils/cronJob.ts
@@ -1,12 +1,35 @@
 import { AsyncStorage } from 'react-native'
 import { NetInfo } from '@react-native-community/netinfo'
 import axios from 'axios'
+import queryString from 'query-string'
+import { IFormVals } from '../screens/Form'
 
-export const cronjob = async (): Promise<T> => {
+const getUrl: string = ''
+const postUrl: string = ''
+
+export const cronjob = async (): void => {
   const isConnected = await checkConnectivity()
   if (!isConnected) return
-  const responses: any[] = getFormResponses()
-  if(!responses.length) return
+  await getAndSend('form-responses', getUrl, 'get')
+  await getAndSend('errors', postUrl, 'post')
+}
+
+const getAndSend = async (asyncStorageKey: string, baseUrl: string, method: string) => {
+  try {
+    const storedVals: string | null = await AsyncStorage.getItem(asyncStorageKey)
+    const parsedVals: IError[] | IFormVals[] = storedVals ? JSON.parse(storedVals) : []
+    if(!parsedVals.length) return
+    parsedVals.forEach(async (v: IError | IFormVals) => {
+      if(method === 'get') {
+        const params = constructParams(v)
+        await axios.get(`${baseUrl}/?${params}`)
+      } else {
+        await axios.post(baseUrl, JSON.stringify(v))
+      }
+    })
+  } catch(err) {
+    storeErrors(err)
+  }
 }
 
 const checkConnectivity = async (): Promise<boolean> => {
@@ -25,6 +48,7 @@ interface IError {
   stack: string
 }
 
+
 export const storeErrors = async (error: Error): Promise<boolean> => {
   try {
     const errors: string | null = await AsyncStorage.getItem('errors')
@@ -42,6 +66,14 @@ export const storeErrors = async (error: Error): Promise<boolean> => {
   }
 }
 
-const getFormResponses = (): Promise<any[]> => {
-
+const constructParams = (response:any): string => {
+  const paramObj = Object.keys(response).reduce((acc: {[key:string]: string}, k) => {
+    const entries = response[k]
+    Object.keys(entries).forEach(subKey => {
+      const topicAndQuestion: string = k + subKey
+      acc[topicAndQuestion] = entries[subKey]
+    })
+    return acc
+  }, {})
+  return queryString.stringify(paramObj)
 }
